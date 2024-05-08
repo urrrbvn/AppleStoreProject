877db4715a5d16add79337503af3acb9b19116fa
Raikirie Inc <sasha.sharifov.2000@mail.ru>
2024-05-08 13:29:25 +0300

    commit

--- src/components/SliderComponent.vue
+++ src/components/SliderComponent.vue
@@ -1,20 +1,60 @@
 <template>
     <div class="slider">
-        <h1>SLIDER HERE</h1>
+      <div
+        v-for="(slide, index) in slides"
+        :key="index"
+        class="slide"
+        :class="{ 'active': currentSlide === index }"
+        :style="{ backgroundColor: slide.color }"
+      >
+        <div class="content">
+          <div class="images-container">
+            <div class="image-container">
+              <img :src="slide.image" alt="Slide {{ index + 1 }} image">
+              <div class="image-container-second">
+                <img :src="slide.secondaryImage" alt="Slide {{ index + 1 }} secondary image">
+                <p>{{ slide.content }}</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button class="prev-btn" @click="prevSlide"><img src="../assets/images/sliderVectorLeft.png" alt="Previous"></button>
+      <button class="next-btn" @click="nextSlide"><img src="../assets/images/sliderVector.png" alt="Next"></button>
     </div>
-</template>
-
-<script setup>
-
-</script>
-
-<style lang="scss" scoped>
-    .slider{
-        height: 480px;
-        background-color: aqua;
-
-        display: flex;
-        align-items: center;
-        justify-content: center;
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const slides = [
+    { 
+      color: 'lightblue', 
+      content: 'от  137 900₽', 
+      image: 'src/assets/images/iphoneSlider.png', 
+      secondaryImage: 'src/assets/images/bannerSlide.png' 
+    },
+    { 
+      color: 'lightblue', 
+      content: 'от 100 900₽', 
+      image: 'src/assets/images/iphoneSlider.png', 
+      secondaryImage: 'src/assets/images/bannerSlide.png' 
+    },
+    { 
+      color: 'lightblue', 
+      content: 'от 237 900₽', 
+      image: 'src/assets/images/iphoneSlider.png', 
+      secondaryImage: 'src/assets/images/bannerSlide.png' 
     }
-</style>
+  ];
+  
+  const currentSlide = ref(0);
+  
+  const nextSlide = () => {
+    currentSlide.value = (currentSlide.value + 1) % slides.length;
+  };
+  
+  const prevSlide = () => {
+    currentSlide.value = (currentSlide.value - 1 + slides.length) % slides.length;
+  };
+  </script>
