ab0b3f26afa65c413f46a89dc7054415c3d82207
Artem Kamaldinov <kamaltema@yandex.ru>
2024-05-09 14:57:03 +0300

    add SocialMediaIcons Ui-Component

--- src/UI/SocialMediaIcons.vue
+++ src/UI/SocialMediaIcons.vue
@@ -1,13 +1,13 @@
 <template>
     <figure class="social-media-icons">
-        <button class="social-media-icons__button" @click="test()">
-            <img class="social-media-icons__img" src="/src/assets/images/whatsappIcon.svg" width="32px" height="32px">
+        <button @click="test()">
+            <img src="/src/assets/images/whatsappIcon.svg" width="32px" height="32px">
         </button>
-        <button class="social-media-icons__button" @click="test()">
-            <img class="social-media-icons__img" src="/src/assets/images/telegramIcon.svg" width="32px" height="32px">
+        <button @click="test()">
+            <img src="/src/assets/images/telegramIcon.svg" width="32px" height="32px">
         </button>
-        <button class="social-media-icons__button" @click="test()">
-            <img class="social-media-icons__img" src="/src/assets/images/vkontakteIcon.svg" width="32px" height="32px">
+        <button @click="test()">
+            <img src="/src/assets/images/vkontakteIcon.svg" width="32px" height="32px">
         </button>
     </figure>
 </template>
@@ -25,11 +25,4 @@ function test(){
     gap: 8px;
 }
 
-.social-media-icons__button{
-    background-color: transparent;
-    border: none;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
 </style>
