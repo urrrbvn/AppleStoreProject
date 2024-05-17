141825408459be46c4877cc68699b3d3ea7ef140
Олег  Трушнин <truoleg2004@gmail.com>
2024-05-17 22:25:35 +0300

    minor commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <HeaderComponent v-if="windowWidth > 1200"/>
   <TabletHeaderComponent v-if="windowWidth < 1200 && windowWidth > 768" />
-  <RouterView></RouterView>
-  <FooterComponent></FooterComponent>
+  <!-- <RouterView></RouterView> -->
+  <MobileMenuComponent/>
+  <!-- <FooterComponent></FooterComponent> -->
 </template>
 
 <script setup>
@@ -10,7 +11,9 @@ import HeaderComponent from './components/HeaderComponent.vue';
 import { RouterView } from 'vue-router';
 import FooterComponent from './components/FooterComponent.vue'
 import { ref, onMounted, onUnmounted } from 'vue';
-import TabletHeaderComponent from './components/TabletHeaderComponent.vue'
+import TabletHeaderComponent from './components/TabletHeaderComponent.vue';
+import MobileMenuComponent from './components/MobileMenuComponent.vue';
+
 
 
 const windowWidth = ref(window.innerWidth);
---
+++ src/components/MobileMenuComponent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="mobile-menu">
+        
+    </div>
+</template>
+
+<script setup>
+import { onMounted, reactive, ref } from 'vue';
+import IconButton from '../UI/IconButton.vue';
+
+</script>
